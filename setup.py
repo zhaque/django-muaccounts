11371da2495b5aabc0989f242acfef6b9eb03130
Sidorenko Vladimir <yoyavova@gmail.com>
2009-10-05 15:29:05 +0300

    basic setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name="django-muaccaunts",
+           version="0.1",
+           description="Domain-based multi-user accounts",
+           author="CrowdSense",
+           author_email="admin@crowdsense.com",
+           packages=find_packages(),
+           include_package_data=True,
+)
+
