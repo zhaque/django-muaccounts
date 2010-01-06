dc8f3b00dec731b548a8b8cd24ac36320105ae34
SaaSkit <saaskit@DevBook.home>
2010-01-06 20:20:11 +0000

    License Fix

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(name="django-muaccaunts",
            version="0.1.2",
            description="Domain-based multi-user accounts",
-           author="CrowdSense",
-           author_email="admin@crowdsense.com",
+           author="SaaS kit",
+           author_email="admin@saaskit.org",
            packages=find_packages(),
            include_package_data=True,
 )
